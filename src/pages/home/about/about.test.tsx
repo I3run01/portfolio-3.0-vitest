328d76b862f60b08adfdb266e0a04f5aa1ba25be
I3run01 <brunnooa.v@gmail.com>
2024-05-16 11:46:33 -0300

    tests

---
+++ src/pages/home/about/about.test.tsx
@@ -0,0 +1,28 @@
+import { fireEvent} from '@testing-library/react';
+import { About } from '.';
+import { beforeEach, describe, expect, it } from 'vitest';
+import { renderWithProviders } from 'src/__test__/renderWithProviders';
+
+describe('<About />', () => {
+  let flexBox: HTMLElement;
+
+  beforeEach(() => {
+    const aboutRender = renderWithProviders(<About />);
+    flexBox = aboutRender.getByTestId('flex-box');
+
+    global.innerWidth = 1980;
+    fireEvent(window, new Event('resize'));
+  });
+
+  it('should have flex-direction: row', () => {
+    expect(flexBox).toHaveStyle('flex-direction: row');;
+  });
+
+  it('should have the flex-box like column', async () => {
+    global.innerWidth = 400;
+    fireEvent(window, new Event('resize'));
+
+    expect(flexBox).toHaveStyle('flex-direction: column');
+  });
+
+});
